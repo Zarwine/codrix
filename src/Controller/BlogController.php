d97992fd63417ac2d3f55ef1ac2fb395194990c3
Zarwine <ange_550@hotmail.com>
2020-09-08 14:24:27 +0200

    Lecture et affichage de donnés stockées en BDD

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,16 +3,21 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 
 class BlogController extends AbstractController 
 {
     /**
      * @Route("/blog", name="blog")
      */
-    public function index()
+    public function index(ArticleRepository $repo)
     {
+        $articles = $repo->findAll();
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'articles' => $articles
         ]);
     }
     
@@ -27,10 +32,20 @@ class BlogController extends AbstractController
     }
 
     /**
-     * @Route("/blog/12", name="blog_show")
+     * @Route("/blog/new", name="blog_create")
      */
-    public function show() 
+    public function create() 
     {
-        return $this->render('blog/show.html.twig');
+        return $this->render('blog/create.html.twig');
+    }
+    
+    /**
+     * @Route("/blog/{id}", name="blog_show")
+     */
+    public function show(Article $article) 
+    {;
+        return $this->render('blog/show.html.twig', [
+            "article" => $article
+        ]);
     }
 }
